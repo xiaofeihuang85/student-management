a1d1a6d3b9f8440fcfc69b579a443e75134acc47
xhuang <xiaofeihuang85@gmail.com>
2019-02-13 14:39:13 -0500

    Code refactoring -- put all the code in the functions

--- student-management.py
+++ student-management.py
@@ -1,23 +1,23 @@
 import json
 
 
-def save_students_to_file():
+def save_students_to_file(students):
     with open('students.txt', 'w') as f:
         for item in students:
             f.write("%s\n" % json.dumps(item))
 
 
-def add_student():
+def add_student(students):
     print("===========Add Student===========")
     name = input("Name: ")
     age = input("Age: ")
     major = input("Major: ")
     stu_id = students.__len__()
     students.append({"id": int(stu_id), "name": name, "age": age, "major": major})
-    save_students_to_file()
+    save_students_to_file(students)
 
 
-def delete_student():
+def delete_student(students):
     print("===========Delete Student===========")
     while True:
         stu_id = input("Student Id: ")
@@ -27,7 +27,7 @@ def delete_student():
                 for i in range(stu_id + 1, students.__len__()):
                     students[i]["id"] -= 1
                 print(students.pop(stu_id))
-                save_students_to_file()
+                save_students_to_file(students)
                 break
             else:
                 print("Could not find a student with id: {0}".format(stu_id))
@@ -35,7 +35,7 @@ def delete_student():
             print("Please input a number!")
 
 
-def update_student():
+def update_student(students):
     print("===========Update Student===========")
     while True:
         stu_id = input("Student Id: ")
@@ -49,7 +49,7 @@ def update_student():
                 students[stu_id]["name"] = name
                 students[stu_id]["age"] = age
                 students[stu_id]["major"] = major
-                save_students_to_file()
+                save_students_to_file(students)
                 break
             else:
                 print("Could not find a student with id: {0}".format(stu_id))
@@ -57,7 +57,7 @@ def update_student():
             print("Please input a number!")
 
 
-def find_student():
+def find_student(students):
     print("===========Search Student===========")
     while True:
         stu_id = input("Student Id: ")
@@ -72,47 +72,58 @@ def find_student():
             print("Please input a number!")
 
 
-def show_students():
+def show_students(students):
     print(students)
 
 
-try:
-    students = []
-    with open('students.txt') as f:
-        for line in f.readlines():
-            students.append(json.loads(line))
-except OSError:
-    students = []
-else:
-    f.close()
+def main():
+    print_menu(init_students())
 
-while True:
-    command = input("\
-    =================================================\n\
-              Student Management System\n\
-              1: Add A Student\n\
-              2: Delete A Student\n\
-              3: Update A Student\n\
-              4: Find A Student\n\
-              5: Show Students\n\
-              6: Exit\n\
-    =================================================\n\
-                           Help\n\
-    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n\
-    1:Add|2:Delete|3:Update|4:Find|5:Display All|6:Exit\n\
-    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n\
-    Command:")
-    if command.isdigit():
-        command = int(command)
-        if command == 1:
-            add_student()
-        if command == 2:
-            delete_student()
-        if command == 3:
-            update_student()
-        if command == 4:
-            find_student()
-        if command == 5:
-            show_students()
-        if command == 6:
-            break
+
+def init_students():
+    students = []
+    try:
+        with open('students.txt') as f:
+            for line in f.readlines():
+                students.append(json.loads(line))
+    except OSError:
+        pass
+    else:
+        f.close()
+    return students;
+
+
+def print_menu(students):
+    print("=" * 50)
+    print("\t\t\tStudent Management System")
+    print("\t\t\t1: Add A Student")
+    print("\t\t\t2: Delete A Student")
+    print("\t\t\t3: Update A Student")
+    print("\t\t\t4: Find A Student")
+    print("\t\t\t5: Show Students")
+    print("\t\t\t6: Exit")
+    print("=" * 50)
+    print()
+    while True:
+        print("\t\t\t\tChoose Your Action: ")
+        print("~" * 50)
+        print("1:Add|2:Delete|3:Update|4:Find|5:Display All|6:Exit")
+        print("~" * 50)
+        command = input("Command: ")
+        if command.isdigit():
+            command = int(command)
+            if command == 1:
+                add_student(students)
+            if command == 2:
+                delete_student(students)
+            if command == 3:
+                update_student(students)
+            if command == 4:
+                find_student(students)
+            if command == 5:
+                show_students(students)
+            if command == 6:
+                break
+
+
+main()
